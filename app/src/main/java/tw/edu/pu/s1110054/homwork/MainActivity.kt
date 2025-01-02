5088755828a33b6e9df3668cf009780e48d107f1
colayyun <koyun0902@gmail.com>
2025-01-02 21:53:24 +0800

    洪可芸的第三次Commit

--- app/src/main/java/tw/edu/pu/s1110054/homwork/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1110054/homwork/MainActivity.kt
@@ -17,31 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            HomworkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HomworkTheme {
-        Greeting("Android")
+        //setContentView(R.layout.activity_main)
+        setContentView(Fingers(this))
     }
 }
